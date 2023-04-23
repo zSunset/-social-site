33db9546f7b8902f1f632419f07a3da973787c7b
zSunset <85899844+zSunset@users.noreply.github.com>
2023-04-23 03:01:25 +0300

    chapter_1 (#1)

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+name = "account"
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
